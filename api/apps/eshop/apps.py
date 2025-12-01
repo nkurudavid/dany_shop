e561dc0f97d115a08744ae1f4b45a3ae54be8753
nkurudavid <nkurudavid44@gmail.com>
2025-12-01 22:24:19 +0200

    signal created

--- api/apps/eshop/apps.py
+++ api/apps/eshop/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class EshopConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'apps.eshop'
+    
+    def ready(self):
+        import apps.eshop.controller.signals
